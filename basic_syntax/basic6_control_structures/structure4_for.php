ec611b3a383292876a97566f7a5f3d95473d2aab
Sherry <1690641736@qq.com>
2022-11-22 09:53:15 -0500

    PHP: for loops

---
+++ basic_syntax/basic6_control_structures/structure4_for.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+            /* for Loop */
+            echo "<h1><code>for</code> Loop</h1>";
+
+            for ($num = 1; $num <= 10; $num += 2) {
+                echo "<p>$num</p>";
+            }
+        ?>
+    </body>
+</html>
