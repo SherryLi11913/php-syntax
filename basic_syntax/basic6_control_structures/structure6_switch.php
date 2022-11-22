8d2394f24c16a9e3df4d90642ad1483558ed0ec3
Sherry <1690641736@qq.com>
2022-11-22 10:05:35 -0500

    PHP: switch

---
+++ basic_syntax/basic6_control_structures/structure6_switch.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+            /* switch */
+            echo "<h1><code>switch</code></h1>";
+
+            switch (0) {
+                case 0:
+                    echo "Zero";
+                    break;
+                case 1:
+                    echo "One";
+                    break;
+                default:
+                    echo "Other";
+                    break;
+            }
+            echo "<br/>";
+
+            switch (3) {
+                case 0:
+                    echo "Zero";
+                    break;
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                    echo "Positive integer less than 5";
+                    break;
+                default:
+                    echo "Other";
+                    break;
+            }
+            echo "<br/>";
+
+            switch (-1) {
+                case 0:
+                    echo "Zero";
+                    break;
+                default:
+                    echo "Other";
+                    break;
+            }
+        ?>
+    </body>
+</html>
