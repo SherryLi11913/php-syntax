0d89f945899bc54e8b3bd500bfad8fa934e501cd
Sherry <1690641736@qq.com>
2022-11-22 09:45:10 -0500

    PHP: while loop

---
+++ basic_syntax/basic6_control_structures/structure2_while.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+            /* while Loop */
+            echo "<h1><code>while</code> Loop</h1>";
+
+            $num = 0;
+            while ($num <= 10) {
+                echo "<p>$num</p>";
+                $num ++;
+            }
+        ?>
+    </body>
+</html>
